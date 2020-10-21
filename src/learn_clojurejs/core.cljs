0d0141728d5b626fc4eee3a8cf0464c1ce770c10
Dean Cook <deanrobertcook@gmail.com>
2020-10-21 08:18:23 +0200

    Setup for clojure API tutorial

--- src/learn_clojurejs/core.cljs
+++
@@ -1,2 +0,0 @@
-(ns learn-clojurejs.core)
-
--- src/myproject/welcome.clj
+++
@@ -1,7 +0,0 @@
-(ns myproject.welcome
-  (:require [myproject.person-names :as pnames])) ;; NOTE: `myproject.welcome` requires `myproject.person-names`
-
-(defn greet
-  [first-name last-name]
-  (str "Hello, " (pnames/familiar-name first-name last-name)))
-
